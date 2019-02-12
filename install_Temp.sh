d9393f9adba7512a38e09c7c0c270eb45b30f7d4
PeterMuehler <john.doe.business@gmx.de>
2019-02-12 10:44:41 +0100

    Update install_Temp.sh

--- install_Temp.sh
+++ install_Temp.sh
@@ -2,19 +2,19 @@
 
 
 #creates folder
-mkdir /opt/Temp
+sudo mkdir /opt/Temp
 
 #make executable
 chmod +x Temp.sh
 chmod +x uninstall_Temp.sh
 
 #copies file into opt folder
-cp Temp.sh /opt/Temp
-cp uninstall_Temp.sh /opt/Temp
+sudo cp Temp.sh /opt/Temp
+auso cp uninstall_Temp.sh /opt/Temp
 
 
 #creates link with global variable
-ln -s /opt/Temp/Temp.sh /usr/bin/temp
+sudo ln -s /opt/Temp/Temp.sh /usr/bin/temp
 
 #deletes temp folder after installation
 cd ..
